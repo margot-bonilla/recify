16676b64762f40926e625475b1139eac0153943b
Margot <mbonilla@trivago.com>
2018-12-21 18:06:09 +0100

    main function

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package recify
+package main
 
 import (
 	"fmt"
@@ -6,25 +6,57 @@ import (
 	"net/http"
 	"os"
 	"recify/controllers"
+	"strings"
 )
 
+func sayHello(res http.ResponseWriter, req *http.Request) {
+	message := req.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	res.Write([]byte(message))
+}
+
+//func main() {
+//
+//	router := mux.NewRouter()
+//	//router.Use(app.JwtAuthentication) //attach JWT auth middleware
+//
+//	router.HandleFunc("/", sayHello)
+//
+//	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+//	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+//
+//	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+//	if port == "" {
+//		port = "8000" //localhost
+//	}
+//
+//	fmt.Println(port)
+//
+//	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+//	if err != nil {
+//		fmt.Print(err)
+//	}
+//}
+
 func main() {
+		router := mux.NewRouter()
+		//router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-	router := mux.NewRouter()
-	//router.Use(app.JwtAuthentication) //attach JWT auth middleware
+		router.HandleFunc("/", sayHello)
 
-	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
-	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+		router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+		router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 
-	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
-	if port == "" {
-		port = "8000" //localhost
-	}
+		port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+		if port == "" {
+			port = "8000" //localhost
+		}
 
-	fmt.Println(port)
+		fmt.Println(port)
 
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
-	if err != nil {
-		fmt.Print(err)
-	}
+		err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+		if err != nil {
+			fmt.Print(err)
+		}
 }
