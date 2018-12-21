a3af2f00dcbadb811dd0faab450fc365da116675
Margot <mbonilla@trivago.com>
2018-12-21 17:40:34 +0100

    first structure

---
+++ models/recipe.go
@@ -0,0 +1,64 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"log"
+	u "recify/utils"
+)
+
+type Recipe struct {
+	gorm.Model
+	Title string `json:"title"`
+	Steps string `json:"steps"`
+	Description string `json:"description"`
+	//IngredientList []Ingredient `gorm:foreignKey:RecipeRefer`
+}
+
+/*
+ This struct function validate the required parameters sent through the http request body
+returns message and true if the requirement is met
+*/
+func (recipe *Recipe) Validate() (map[string] interface{}, bool) {
+
+	//All the required parameters are present
+	return u.Message(true, "success"), true
+}
+
+
+
+func (recipe *Recipe) Create() map[string] interface{} {
+
+	if resp, ok := recipe.Validate(); !ok {
+		return resp
+	}
+
+	GetDB().Create(recipe)
+
+	resp := u.Message(true, "success")
+	resp["recipe"] = recipe
+	return resp
+}
+
+func GetRecipe(id uint) *Recipe {
+	recipe := &Recipe{}
+	err := GetDB().Table("recipe").Where("id = ?", id).First(recipe).Error
+
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return recipe
+}
+
+func GetRecipes() []*Recipe {
+	recipes := make([]*Recipe, 0)
+	err := GetDB().Table("recipe").Find(&recipes).Error
+
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return recipes
+}
---
+++ models/user.go
@@ -0,0 +1,123 @@
+package models
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	u "recify/utils"
+	"strings"
+	"github.com/jinzhu/gorm"
+	"os"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const UserTableName string = "recify_user";
+
+/*
+JWT claims struct
+*/
+type Token struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+//a struct to rep user
+type User struct {
+	gorm.Model
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Token string `json:"token";sql:"-"`
+}
+
+//Validate incoming user details...
+func (user *User) Validate() (map[string] interface{}, bool) {
+
+	if !strings.Contains(user.Email, "@") {
+		return u.Message(false, "Email address is required"), false
+	}
+
+	if len(user.Password) < 6 {
+		return u.Message(false, "Password is required"), false
+	}
+
+	//Email must be unique
+	temp := &User{}
+
+	//check for errors and duplicate emails
+	err := GetDB().Table(UserTableName).Where("email = ?", user.Email).First(temp).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return u.Message(false, "Connection error. Please retry"), false
+	}
+	if temp.Email != "" {
+		return u.Message(false, "Email address already in use by another user."), false
+	}
+
+	return u.Message(false, "Requirement passed"), true
+}
+
+func (user *User) Create() (map[string] interface{}) {
+
+	if resp, ok := user.Validate(); !ok {
+		return resp
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	user.Password = string(hashedPassword)
+
+	GetDB().Create(user)
+
+	if user.ID <= 0 {
+		return u.Message(false, "Failed to create user, connection error.")
+	}
+
+	//Create new JWT token for the newly registered user
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	user.Token = tokenString
+
+	user.Password = "" //delete password
+
+	response := u.Message(true, "User has been created")
+	response[UserTableName] = user
+	return response
+}
+
+func Login(email, password string) (map[string]interface{}) {
+
+	user := &User{}
+	err := GetDB().Table(UserTableName).Where("email = ?", email).First(user).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return u.Message(false, "Email address not found")
+		}
+		return u.Message(false, "Connection error. Please retry")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
+		return u.Message(false, "Invalid login credentials. Please try again")
+	}
+	//Worked! Logged In
+	user.Password = ""
+
+	//Create JWT token
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	user.Token = tokenString //Store the token in the response
+
+	resp := u.Message(true, "Logged In")
+	resp[UserTableName] = user
+	return resp
+}
+
+func GetUser(u uint) *User {
+
+	acc := &User{}
+	GetDB().Table(UserTableName).Where("id = ?", u).First(acc)
+	if acc.Email == "" { //User not found!
+		return nil
+	}
+
+	acc.Password = ""
+	return acc
+}
