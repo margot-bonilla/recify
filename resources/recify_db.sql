add3609c139769d69c2517cf4a22fb163979e208
raul <raul.sanchez@trivago.com>
2018-12-26 13:00:14 +0100

    database model

--- resources/recify_db.sql
+++ resources/recify_db.sql
@@ -1,3 +1,7 @@
+/* recify - db model */
+
+-- users
+
 CREATE TABLE recify_user (
   id SERIAL PRIMARY KEY,
   email VARCHAR(80) NOT NULL UNIQUE,
@@ -6,7 +10,10 @@ CREATE TABLE recify_user (
   updated_at TIMESTAMP NULL
 );
 
-CREATE INDEX recify_user_email_idx ON recify_user (email)
+CREATE INDEX recify_user_email_idx ON recify_user (email);
+
+
+-- groups
 
 CREATE TABLE recify_group (
 	id serial PRIMARY KEY,
@@ -17,29 +24,27 @@ CREATE TABLE recify_group (
 );
 
 
-CREATE TABLE user2group (
+-- users into a group
+
+CREATE TABLE user_to_group (
 	user_id SERIAL NOT NULL,
 	group_id SERIAL NOT NULL,
-	CONSTRAINT user_user2group_fk
+	CONSTRAINT user_id_user_to_group_fk
 	  FOREIGN KEY (user_id)
 		REFERENCES recify_user(id)
 		ON UPDATE CASCADE,
-	CONSTRAINT group_user2group_fk
+	CONSTRAINT group_id_user_to_group_fk
 	  FOREIGN KEY (user_id)
 		REFERENCES recify_group(id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE
 );
 
-CREATE INDEX user_id_user2group_idx ON user2group (user_id)
-CREATE INDEX group_id_user2group_idx ON user2group (group_id)
+CREATE INDEX user_id_user_to_group_idx ON user_to_group (user_id);
+CREATE INDEX group_id_user_to_group_idx ON user_to_group (group_id);
 
-/* example to test group cascade deletion:
-INSERT INTO recify_user VALUES (1, 'raul@email.com', '1234', current_timestamp, NULL)
-INSERT INTO recify_group VALUES (1, 'family','family recipes', current_timestamp, null)
-INSERT INTO user2group VALUES (1,1)
-DELETE FROM recify_group WHERE id = 1
-*/
+
+-- recipes
 
 CREATE TABLE recipe (
 	id serial PRIMARY KEY,
@@ -47,24 +52,117 @@ CREATE TABLE recipe (
 	description VARCHAR(1024)  NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NULL
-)
+);
 
 
-CREATE TABLE group2recipe (
+-- recipes contained into a group
+
+CREATE TABLE group_to_recipe (
 	group_id SERIAL NOT NULL,
 	recipe_id SERIAL NOT NULL,
-	CONSTRAINT group_group2recipe_fk
+	CONSTRAINT group_id_group_to_recipe_fk
 		FOREIGN KEY (group_id)
 		REFERENCES recify_group(id)
 		ON UPDATE CASCADE,
-	CONSTRAINT recipe_group2recipe_fk
+	CONSTRAINT recipe_id_group_to_recipe_fk
 		FOREIGN KEY (recipe_id)
 		REFERENCES recipe(id)
 		ON UPDATE CASCADE
-)
+);
 
-CREATE INDEX recipe_id_group2recipe_idx ON group2recipe (recipe_id)
-CREATE INDEX group_id_group2recipe_idx ON group2recipe (group_id)
+CREATE INDEX recipe_id_group_to_recipe_idx ON group_to_recipe (recipe_id);
+CREATE INDEX group_id_group_to_recipe_idx ON group_to_recipe (group_id);
 
 
-/* WIP */
+-- recipes liked by users
+
+CREATE TABLE user_like_recipe (
+	user_id SERIAL NOT NULL,
+	recipe_id SERIAL NOT NULL,
+	CONSTRAINT user_id_user_like_recipe_fk
+		FOREIGN KEY (user_id)
+		REFERENCES recify_user(id)
+		ON UPDATE CASCADE,
+	CONSTRAINT recipe_id_user_like_recipe_fk
+		FOREIGN KEY (recipe_id)
+		REFERENCES recipe(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+CREATE INDEX recipe_id_user_like_recipe_idx ON user_like_recipe (recipe_id);
+CREATE INDEX user_id_user_like_recipe_idx ON user_like_recipe (user_id);
+
+
+-- list of recipe categories
+
+CREATE TABLE recipe_category (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL UNIQUE,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NULL
+);
+
+
+-- recipe's categories
+
+CREATE TABLE recipe_to_category (
+	recipe_id SERIAL NOT NULL,
+	category_id SERIAL NOT NULL,
+	CONSTRAINT recipe_id_recipe_to_category_fk
+		FOREIGN KEY (recipe_id)
+		REFERENCES recipe(id)
+		ON UPDATE CASCADE,
+	CONSTRAINT category_id_recipe_to_category_fk
+		FOREIGN KEY (category_id)
+		REFERENCES recipe_category(id)
+		ON UPDATE CASCADE
+);
+
+CREATE INDEX recipe_id_recipe_to_category_idx ON recipe_to_category (recipe_id);
+CREATE INDEX category_id_recipe_to_category_idx ON recipe_to_category (category_id);
+
+
+-- ingredients
+
+CREATE TABLE ingredient (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL UNIQUE,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NULL
+);
+
+-- recipe's amount of ingredients
+
+CREATE TABLE recipe_amount_ingredient (
+	recipe_id SERIAL NOT NULL,
+	ingredient_id SERIAL NOT NULL,
+	amount REAL NOT NULL,
+	measure CHAR(2) NOT NULL,
+	CONSTRAINT recipe_id_recipe_amount_ingredient_fk
+		FOREIGN KEY (recipe_id)
+		REFERENCES recipe(id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	CONSTRAINT ingredient_id_recipe_amount_ingreadient_fk
+		FOREIGN KEY (ingredient_id)
+		REFERENCES ingredient(id)
+		ON UPDATE CASCADE
+);
+
+CREATE INDEX recipe_id_recipe_amount_ingredient_idx ON recipe_amount_ingredient (recipe_id);
+CREATE INDEX ingredient_id_recipe_amount_ingredient_idx ON recipe_amount_ingredient (ingredient_id);
+
+
+-- recipe's steps
+
+CREATE TABLE recipe_step (
+	recipe_id SERIAL NOT NULL,
+	position SMALLSERIAL NOT NULL,
+	description TEXT NOT NULL,
+	PRIMARY KEY (recipe_id, position),
+	CONSTRAINT recipe_id_recipe_step_fk
+		FOREIGN KEY (recipe_id)
+		REFERENCES recipe(id)
+		ON DELETE CASCADE
+);
